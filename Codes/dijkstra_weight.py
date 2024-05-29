12d53b9a7fc89087723898646a7e60a9e0d1281d
stalring1 <starling01@dgu.ac.kr>
2024-05-29 13:27:09 +0900

    add weight

--- Codes/dijkstra_weight.py
+++ Codes/dijkstra_weight.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import numpy as np
 import os
+from make_weight_df import df_weight_dic
 
-print(os.getcwd)
+print(os.getcwd())
 
 class Graph:
-    def __init__(self, distance_matrix):
+    def __init__(self, distance_matrix, weight_matrix):
         self.graph = distance_matrix
+        self.weight_dicts = weight_matrix
         self.V = len(distance_matrix)
     
     def min_distance(self, dist, spt_set):
+        # 최소 거리를 찾는 함수
         min = float('inf')
         min_index = -1
         for v in range(self.V):
@@ -19,62 +22,54 @@ class Graph:
         return min_index
 
     def dijkstra(self, src):
+        # Dijkstra 알고리즘을 사용하여 최단 경로를 계산하는 함수
         dist = [float('inf')] * self.V
         dist[src] = 0
         spt_set = [False] * self.V
         prev = [None] * self.V
+
+        weight_index = src//10
+        weight_matrix = self.weight_dicts[weight_index].to_numpy() if weight_index in self.weight_dicts else np.ones((self.V, self.V))
 
         for cout in range(self.V):
             u = self.min_distance(dist, spt_set)
             spt_set[u] = True
             for v in range(self.V):
                 if (self.graph[u][v] > 0 and not spt_set[v] and 
-                    dist[v] > dist[u] + self.graph[u][v] and self.graph[u][v] != 99999):
-                    dist[v] = dist[u] + self.graph[u][v]
+                    dist[v] > dist[u] + self.graph[u][v] * weight_matrix[u][v] and self.graph[u][v] != 99999):
+                    dist[v] = dist[u] + self.graph[u][v] * weight_matrix[u][v]
                     prev[v] = u                
         return dist, prev
     
     def print_path(self, prev, j):
-        # Base Case : If j is source
+        # 경로를 출력하는 함수
         if prev[j] is None:
             print(j, end=' ')
             return
         self.print_path(prev, prev[j])
         print(j, end=' ')
 
-# Create a DataFrame from the provided distance matrix
+# 파일 경로
 file_path = './ExcelFiles/DistanceBtwnNodes.xlsx'
 data_DBN = pd.read_excel(file_path, index_col=0)
 df_DBN = pd.DataFrame(data_DBN)
 
-# Convert the DataFrame to a numpy array for graph processing
-distance_matrix = df_DBN.to_numpy()
+# Graph 객체 생성
+graph_DBN = Graph(df_DBN.to_numpy(), df_weight_dic)
 
-# 출구의 index 추출하기&출구노드 분리를 위한 열 이름 문자열 변환
-graph_DBN = Graph(distance_matrix)
+# 출구 노드 분리를 위한 열 이름 문자열 변환
 column_name_S = df_DBN.columns.map(str)
-
-#출구와 출구가 아닌 노드의 index 분리
 exits = [node for node in column_name_S if node.startswith('출구')]
 exits_index = [df_DBN.columns.get_loc(col) for col in exits]
-non_exit_indices = [i for i in range(380) if i not in exits_index]
-
-#경로를 저장할 위치
-shortest_paths = {}
+non_exit_indices = [i for i in range(graph_DBN.V) if i not in exits_index]
 
 # Compute the shortest paths to each exit for each node excluding the exits themselves
-for index_node in non_exit_indices: 
-    path = []
-    node_name = df_DBN.index[index_node]
+shortest_paths = {}
+for index_node in non_exit_indices:
     distances, predecessors = graph_DBN.dijkstra(index_node)
     closer_exit = min((distances[exit], exit) for exit in exits_index)
-    shortest_paths[df_DBN.columns[index_node]] = closer_exit
-    path = graph_DBN(predecessors, closer_exit[1])
-    print(predecessors, "\n")
-    print(f"Shortest path from {node_name} to {closer_exit[1]} is {closer_exit[0]} units.", end="")
+    node_name = df_DBN.index[index_node]
+    print(f"Shortest path from {node_name} to {df_DBN.columns[closer_exit[1]]} is {closer_exit[0]} units.", end="")
     print(" path : ", end="")
     graph_DBN.print_path(predecessors, closer_exit[1])
     print("\n")
-
-print(shortest_paths)
-
--- Codes/try.py
+++ Codes/try.py
@@ -1,5 +1,26 @@
+import simpy
 import pandas as pd
-import numpy as np
 
-file_path = './ExcelFiles/DistanceBtwnNodes.xlsx'
-data_DBN = pd.read_excel(file_path, index_col=0)
+file_path_distance = './ExcelFiles/DistanceBtwnNodes.xlsx'
+distance_data = pd.read_excel(file_path_distance, index_col=0)
+distance_df = pd.DataFrame(distance_data)
+
+file_path_routes = './ExcelFiles/shortestpaths.csv'
+routes_data = pd.read_csv(file_path_routes)
+routes_df = pd.DataFrame(routes_data)
+
+file_path_people = './ExcelFiles/Last_info_original.xlsx'
+people_data = pd.read_excel(file_path_people)
+people_df = pd.DataFrame(people_data)
+
+people_routes_df = pd.merge(people_df, routes_df, on='Node', how='left')
+
+# 0이 아닌 값만 선택
+non_zero_values = distance_df[(distance_df != 0) & (distance_df != 9999999999)]
+
+# 0이 아닌 값들의 평균 계산
+mean_value = non_zero_values.stack().mean()
+max_value = non_zero_values.max().max()
+min_value = non_zero_values.min().min()
+mid_value = non_zero_values.stack().median()
+print("평균:", mean_value, "max", max_value, "min", min_value, "mid", mid_value)
